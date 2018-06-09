de0a0f99178b3ccc729e3b8e46a82ca2bcf2cf82
Oleg Panasevych <oleg@MacBook-Air-Oleg.local>
2018-06-10 02:11:08 +0300

    Some interface updates

--- static/app.js
+++ static/app.js
@@ -1,12 +1,27 @@
 var app = angular.module("app", []);
 
 app.controller("MainController", function($scope, $http) {
+  var itemsPerPage = 3;
+
   // Get artists
-  $http.get('/artists/browse/', {params: {page_size: 3}} ).then(function(response){
+  $http.get('/artists/browse/', {params: {page_size: itemsPerPage}} ).then(function(response){
     $scope.artists = response.data.results;
   });
   // Get releases
-  $http.get('/releases/browse/', {params: {page_size: 3}} ).then(function(response){
+  $http.get('/releases/browse/', {params: {page_size: itemsPerPage}} ).then(function(response){
     $scope.releases = response.data.results;
   });
+
+  $scope.search = function(query) {
+
+     // Get artists
+     $http.get('/artists/browse/', {params: {search: query, page_size: itemsPerPage}} ).then(function(response){
+       $scope.artists = response.data.results;
+     });
+     // Get releases
+     $http.get('/releases/browse/', {params: {search: query, page_size: itemsPerPage}} ).then(function(response){
+       $scope.releases = response.data.results;
+     });
+
+   };
 });
